86a9b8f5bc3dd88e1514bb5e94db26e4cb6efd2f
Denis Murphy <djmurphy2001@gmail.com>
2021-11-16 18:07:54 -0330

    first commit of JS and CSS files

---
+++ jquery.validate_plugin/jquery.validate-plugin.js
@@ -0,0 +1,12 @@
+(function($) {
+    $.fn.validate = function(options) {
+
+        let settings = $.extend({
+
+        }, options)
+
+        return this.each(function() {
+            
+        })
+    }
+}(jQuery));
