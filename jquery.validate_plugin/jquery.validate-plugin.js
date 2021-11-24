330bee34aa151d09673134aad2b51e0c094bb8e2
Denis Murphy <djmurphy2001@gmail.com>
2021-11-24 18:53:18 -0330

    Updated validation framework to allow for mutliply fields of each type, also added test html and script

--- jquery.validate_plugin/jquery.validate-plugin.js
+++ jquery.validate_plugin/jquery.validate-plugin.js
@@ -1,118 +1,131 @@
 (function($) {
-    $.fn.validate = function(options) {
+    $.fn.validator = function(options) {
 
         let settings = $.extend({
-            textId = null,
-            textMin = 1,
-            textMax = "max",
+            type: null,
 
-            numberId = null,
-            numberMin = 1,
-            numberMax = "max",
-            numberNeg = true,
-
-            phoneId = null,
-            phonePattern = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im,
-
-            emailId = null,
-            emailPattern = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}\b/,
-
-            passwordId = null,
-            matchId = null,
-
-            passwordMin = 1,
-            passwordMax = "max",
-            passwordCaps = true,
-            passwordSymbols = true,
-            passwordNumbers = true
-
+            min: 1,
+            max: "max",
+            neg: true,
+            caps: true,
+            symbols: true,
+            numbers: true,
+            phonePattern: /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im,
+            emailPattern: /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}\b/,
+            matchId: null,
         }, options)
 
         return this.each(function() {
-            
-            let isValid = true;
+            // Get id of element
+            const id = `#${this.getAttribute('id')}`
 
-            // Text validator
-            const textId = settings.textId;
-            if (text != null) {
-                const text = $(textId).val().trim();
-                if (text == "") {
-                    $(textId).next().text("This field is required.");
-                }
-                else if (textMax != "max") {
-                    if (text.length > textMax) {
-                        $(textId).next().text(`This field must have no more than ${textMax} characters.`);
+            $("#submit").click( evt => {
+
+                let isValid = true;
+
+                // Text validator
+                if (settings.type == "text") {
+                    const text = $(id).val().trim();
+                    if (text == "") {
+                        $(id).next().text("This field is required.");
+                    }    
+                    else if (settings.max != "max") {
+                        if (text.length > settings.max) {
+                            $(id).next().text(`This field must have no more than ${settings.max} characters.`);
+                        }
                     }
+                    else if (text.length < settings.min) {
+                        $(id).next().text(`This field must have at least ${settings.min} characters.`);
+                    }
+                    else {
+                        $(id).next().text("");
+                    }
+                    $(id).val(text);
+                    
                 }
-                else if (text.length < textMin) {
-                    $(textId).next().text(`This field must have at least ${textMin} characters.`);
+                // Number validator
+                else if (settings.type == "number") {
+                    const number = $(id).val().trim();
+                    if (number == "") {
+                        $(id).next().text("This field is required.");
+                        isValid = false;
+                    }
+                    else if (isNaN(number)) {
+                        $(id).next().text("Must be numeric.");
+                        isValid = false;
+                    }
+                    else if (settings.max != "max") {
+                        if (number.length > settings.max) {
+                            $(id).next().text(`This field must have no more than ${settings.max} characters.`);
+                        }
+                    }
+                    else if (number.length < settings.min) {
+                        $(id).next().text(`This field must have at least ${settings.max} characters.`);
+                    }
+                    else {
+                        $(number).next().text("");
+                    }
+                    $(id).val(number);              
                 }
-                else {
-                    $(textId).next().text("");
+                // Phone validator
+                else if (settings.type == "phone") {
+                    const phone = $(id).val().trim();
+                    if (phone == "") {
+                        $(id).next().text("This field is required.");
+                        isValid = false;
+                    }
+                    else if (!phonePattern.test(phone)) {
+                        $(id).next().text("Must be a valid phone number.");
+                    }
+                    else {
+                        $(phone).next().text("");
+                    }
+                    $(id).val(phone);
+                    }
+                // Email validator 
+                else if (settings.type == "email") {    
+                    const email = $(id).val().trim();
+                    if (email == "") {
+                        $(id).next().text("This field is required.");
+                        isValid = false;
+                    }
+                    else if (!emailPattern.test(email)) {
+                        $(id).next().text("Must be a valid email address.");
+                        isValid = false;
+                    }
+                    else {
+                        $(id).next().text("");
+                    }
+                    $(id).val(email);
                 }
-                $(textId).val(text);
-            }
+                // Password validator
+                else if (settings.type == "password") {            
+                    const password = $(id).val().trim();
+                    if (password == "") {
+                        $(id).next().text("This field is required.");
+                    }
+                    else if (settings.max != "max") {
+                        if (text.length > settings.max) {
+                            $(id).next().text(`This field must have no more than ${settings.max} characters.`);
+                        }
+                    }
+                    else if (text.length < settings.min) {
+                        $(id).next().text(`This field must have at least ${settings.min} characters.`);
+                    }
+                    else {
+                        $(id).next().text("");
+                    }
+                    $(id).val(password);
+                }
+    
+                if (settings.matchId != null) {
+                    const match = $(settings.matchId).val().trim();
+                }
 
-            // Number validator
-            const numberId = settings.numberId;
-            if (numberId != null) {
-                const number = $(numberId).val().trim();
-                if (number == "") {
-                    $(numberId).next().text("This field is required.");
-                    isValid = false;
+                if (isValid == false) {
+                    evt.preventDefault();
                 }
-                else if (isNaN(number)) {
-                    $(numberId).next().text("Must be numeric.");
-                    isValid = false;
-                }
-                else {
-                    $(number).next().text("");
-                }
-                $(numberId).val(number);
-            }
-
-            // Phone Number validator
-            const phoneId = settings.phoneId;
-            if (phoneId != null) {
-                const phone = $(phoneId).val().trim();
-                if (phone == "") {
-                    $(phoneId).next().text("This field is required.");
-                    isValid = false;
-                }
-                else if (!phonePattern.test(phone)) {
-                    $(phoneId).next().text("Must be a valid phone number.");
-                }
-                else {
-                    $(phone).next().text("");
-                }
-                $(phoneId).val(phone);
-            }
-
-            // Email validator
-            const emailId = settings.emailId;
-            if (emailId != null) {
-                const email = $(emailId).val().trim();
-                if (email == "") {
-                    $(emailId).next().text("This field is required.");
-                    isValid = false;
-                }
-                else if (!emailPattern.test(email)) {
-                    $(emailId).next().text("Must be a valid email address.");
-                    isValid = false;
-                }
-                else {
-                    $(emailId).next().text("");
-                }
-                $(emailId).val(email);
-            }
-            // Password validator
-            const passwordId = settings.passwordId;
-            const matchId = settings.matchId;
-
-
-            if (isValid == false) {
-                evt.preventDefault();
-            }
+            })  
         })
     }
 }(jQuery));
---
+++ script.js
@@ -0,0 +1,13 @@
+    $("#text").validator({
+        type: "text"
+    });
+
+    $("#text2").validator({
+        type: "text"
+    });
+
+    $("#email").validator({
+        type: "email"
+    })
+
+    
